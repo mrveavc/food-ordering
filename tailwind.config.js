0c959a995e8a3edf8d1b8bbedf683b6ae21b350e
Merve <mrveavc12@gmail.com>
2022-11-27 19:02:36 +0300

    second-commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {colors:{
+      primary :"#ffbe33;",
+      secondary: "#ffbe33;"
+    },
+    fontFamily:{
+      dancing:["Dancing Script","cursive"],
+    }
+  },
   },
   plugins: [],
 }
