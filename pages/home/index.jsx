cf333a8ab7f4220291a4eb1fc1fe1af71df54e36
Merve <mrveavc12@gmail.com>
2022-12-17 20:18:21 +0300

    campaigns-commit

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Campaigns from '../components/ui/Campaigns';
 import Carousel from '../components/ui/Carousel';
 
  const Index = () => {
   return (
-    <div className='relative'>
+    <div >
       <Carousel/>
+      <Campaigns/>
     </div>
   )
 }
