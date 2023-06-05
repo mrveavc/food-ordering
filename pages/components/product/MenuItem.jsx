b439c35d32331b9e8f28ed3f22ec7a31db305edf
mrveavc <mrveavc12@gmail.com>
2023-06-05 20:52:20 +0300

    About page

---
+++ pages/components/product/MenuItem.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React from "react";
+import { RiShoppingCart2Fill } from "react-icons/ri";
+
+const MenuItem = () => {
+  return (
+    <div className="bg-secondary">
+      <div className="w-full bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px]">
+        <div className="relative w-36 h-36">
+          <Image src="/images/f1.png" alt="" layout="fill" />
+        </div>
+      </div>
+      <div className="p-[25px] text-white">
+        <h4 className="text-xl font-semibold">Delicious Pizza</h4>
+        <p className="text-[15px]"></p>
+        <p>
+          Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit,
+          magnam voluptatem repellendus sed eaque
+        </p>
+        <div className="flex justify-between">
+          <span>$20</span>
+          <button className="btn-primary !w-10 !h-10 !rounded-full p-0">
+            <RiShoppingCart2Fill></RiShoppingCart2Fill>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Campaigns from '../components/ui/Campaigns';
 import Carousel from '../components/ui/Carousel';
+import MenuWrapper from '../components/product/MenuWrapper';
 
  const Index = () => {
   return (
     <div >
       <Carousel/>
       <Campaigns/>
+      <MenuWrapper/>
     </div>
   )
 }
